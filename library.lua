a00b8fcc789e79733b5f8e3324a5a977a8ed9264
Gamer-3 <65517197+Gamer-3@users.noreply.github.com>
2024-11-24 06:45:09 -0500

    Update library.lua

--- library.lua
+++ library.lua
@@ -895,10 +895,9 @@ function Library:create(options)
 	})
 
 	rawset(mt, "creditsContainer", creditsTab.container)
-
+	creditsTab:credit{Name = "luckyCamen", Description = "Script Creator"}
 	creditsTab:credit{Name = "Abstract", Description = "UI Library Developer", Discord = "Abstract#8007", V3rmillion = "AbstractPoo"}
 	creditsTab:credit{Name = "Deity", Description = "UI Library Developer", Discord = "Deity#0228", V3rmillion = "0xDEITY"}
-	creditsTab:credit{Name = "Repository", Description = "UI Library Repository", Github="https://github.com/deeeity/Elysium-lib/blob/master/src.lua"}
 
 	return mt
 end
